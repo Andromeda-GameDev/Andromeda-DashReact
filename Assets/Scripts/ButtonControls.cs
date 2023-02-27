25362097c37ce98030f301b94b15e376b018b55a
Eduardo AG <edag4000@gmail.com>
2023-02-27 12:30:11 -0600

    Agregando assets de botones y mejorando controles de objetos AR

--- Assets/Scripts/ButtonControls.cs
+++ Assets/Scripts/ButtonControls.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonControls : MonoBehaviour
 {
     // Object to transform
-    public GameObject prefab;
+    GameObject prefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +17,13 @@ public class ButtonControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (prefab == null)
+        {
+            prefab = GameObject.FindWithTag("Prefab");
+        }
     }
 
-    // Button Effects
+    // Button Effects Rotations
     public void RotateRight()
     {
         prefab.transform.Rotate(0, 10, 0, Space.World);
@@ -29,6 +33,18 @@ public class ButtonControls : MonoBehaviour
     public void RotateLeft()
     {
         prefab.transform.Rotate(0, -10, 0, Space.World);
+        print("Rotate Left");
+    }
+
+    public void RotateClockwise()
+    {
+        prefab.transform.Rotate(0, 0, 10, Space.World);
+        print("Rotate Right");
+    }
+
+    public void RotateCounterClockwise()
+    {
+        prefab.transform.Rotate(0, 0, -10, Space.World);
         print("Rotate Left");
     }
 }
