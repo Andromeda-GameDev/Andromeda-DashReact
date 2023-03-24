6815ded65104c3ffa1806860a23a0e71592d2c78
Abraham Chalita Núñez <abrahamchalita@hotmail.com>
2023-03-24 01:39:09 -0600

    Modified sidebar behaviour. Shows current page

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,8 +1,49 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import logo from "../resources/openai-logo.png";
+
+function SidebarLink({ to, label }) {
+    const location = useLocation();
+    const isActive = location.pathname === to;
+
+    let iconClass = '';
+    switch (label) {
+        case 'Inicio':
+            iconClass = 'bx bx-home';
+            break;
+        case 'Grupos':
+            iconClass = 'bx bx-spreadsheet';
+            break;
+        case 'Estadísticas':
+            iconClass = 'bx bx-bar-chart';
+            break;
+        default:
+            iconClass = 'bx bx-home';
+    }
+  
+    return (
+      <li>
+        <NavLink
+          to={to}
+          className={`flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 ${
+            isActive ? "text-gray-800 bg-indigo-100" : "text-gray-500 hover:text-gray-800"
+          }`}
+        >
+          <span
+            className={`inline-flex items-center justify-center h-12 w-2 ${
+              isActive ? "bg-indigo-500" : ""
+            }`}
+          />
+          <span className="inline-flex items-center justify-center h-12 w-10 text-lg text-gray-400">
+            <i className={iconClass}></i>
+          </span>
+          <span className="text-sm font-medium">{label}</span>
+        </NavLink>
+      </li>
+    );
+  }
 
 
 export default function Sidebar() {
@@ -34,30 +75,10 @@ export default function Sidebar() {
                     <h1 className="text-2xl text-indigo-500">Andromeda</h1>
                 </div>
                 <ul className="flex flex-col py-4">
-                <li>
-                    <NavLink to='/professor' className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                    <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-home"></i></span>
-                    <span className="text-sm font-medium">Home</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/professor/groups' className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                    <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-spreadsheet"></i></span>
-                    <span className="text-sm font-medium">Groups</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/professor/statistics' className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                    <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-bar-chart"></i></span>
-                    <span className="text-sm font-medium">Statistics</span>
-                    </NavLink>
-                </li>
-                {/* <li>
-                    <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                    <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-cog"></i></span>
-                    <span className="text-sm font-medium">Settings</span>
-                    </a>
-                </li> */}
+
+                <SidebarLink to="/professor" label="Inicio" />
+                <SidebarLink to="/professor/groups" label="Grupos" />
+                <SidebarLink to="/professor/statistics" label="Estadísticas" />
                 <li>
                     <button onClick={handleLogout} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                     <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i className="bx bx-log-out"></i></span>
