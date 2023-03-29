09d152e510f46ad15898d53ac5a12da4991ca0fa
Héctor Miranda García <h3cth0r@Hectors-MacBook-Pro.local>
2023-03-29 12:01:56 -0600

    basic layout students panel

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import StudentSideBar from "../components/StudentSideBar";
 
 const Student = () => {
 
@@ -19,11 +20,16 @@ const Student = () => {
     }
 
     return (
-        <div className = 'max-w-[600px] mx-auto my-16 p-4'> 
-            <h1 className = 'text-2xl font-bold'> Panel de estudiante </h1>
-            <p> User Email: {user && user.email} </p>
+        <div className = 'flex bg-gray-100'> 
+            <div className="flex">
+                <StudentSideBar />
+            </div>
+            <div>
+                <h1 className = 'text-2xl font-bold'> Panel de estudiante </h1>
+                <p> User Email: {user && user.email} </p>
+                <button onClick={handleLogout} className='border px-6 py-2 my-4'> Logout </button>
+            </div>
 
-            <button onClick={handleLogout} className='border px-6 py-2 my-4'> Logout </button>
         </div>
     );
 }
