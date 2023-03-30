c0e734966bf7376c8185185a842801790e369e19
Abraham Chalita <abrahamchalita@hotmail.com>
2023-03-30 13:33:46 -0600

    Updated table connection to firebase and styling

--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 import Sidebar from "../components/sidebar";
-import BarChartAvgTime from "../components/BarChartAvgTime";
-import BarChartAvgScore from "../components/BarChartAvgScore";
 import GeneralStatsRow from "../components/GeneralStatsRow";
 import { CSVLink } from "react-csv";
 import DropdownComponent from "../components/DropdownComponent";
 import HeaderPanel from "../components/headerPanel";
+import { UserAuth } from "../context/AuthContext";
+import {ref, getDatabase, onValue, get} from "firebase/database";
 
 const Statistics = () => {
 
-    const [table1, setTable1] = React.useState([]);
-    const [table2, setTable2] = React.useState([]);
-    const [table3, setTable3] = React.useState([]);
+    const {user} = UserAuth();
+    const [table_progress, setTableProgress] = useState([]);
+    const [selectedOption, setSelectedOption] = useState("");
+
+    const getProfessorGroups = () => {
+        const database = getDatabase();
+        const userRef = ref(database, 'professors/' + user.uid + '/groups');
+        const profGroups = []
+        
+        onValue(userRef, (snapshot) => {
+            const groups = snapshot.val();
+            for(let group in groups) {
+                profGroups.push(groups[group].name);
+            }
+
+            profGroups.push("General")
+
+        });
+        
+        //setGroups(profGroups);
+        return profGroups;
+    }
+
+    const handleSelectedOptionChange = (option) => {
+        setSelectedOption(option);
+    };
 
     return (
+
         <div className="flex bg-gray-100">
 
             <Sidebar />
@@ -26,25 +50,16 @@ const Statistics = () => {
             
             <div className="flex pt-5 pr-9">
                 <div className="ml-8"> 
-                    <DropdownComponent /> 
+                <DropdownComponent assignedGroups={getProfessorGroups()} onSelectedOption={handleSelectedOptionChange} />
                 </div>
-                <CSVLink data={table3} filename={"estadisticas_generales.csv"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full ml-auto"> 
+
+                <CSVLink data={table_progress} filename={"estadisticas_generales.csv"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full ml-auto"> 
                     Descargar CSV 
                 </CSVLink>
             </div>
 
 
             <div className="flex flex-wrap p-5">
-                <div className="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/4">
-                    <div className="block max-w-sm rounded-lg bg-white p-3 shadow-lg mb-8">
-                        <h2 className="text-1xl font-semibold text-gray-800 pb-5 text-center">Tiempo promedio por sección </h2>
-                        <BarChartAvgTime setData={setTable1} /> 
-                    </div>
-                    <div className="block max-w-sm rounded-lg bg-white p-3 shadow-lg">
-                        <h2 className="text-1xl font-semibold text-gray-800 pb-5 text-center">Puntaje promedio por sección</h2>
-                        <BarChartAvgScore setData={setTable2}/>
-                    </div>
-                </div>
                 
                 <div className="col-span-full xl:col-span-6 bg-white shadow-lg rounded-sm border border-slate-200 flex-1">
                     <header className="px-5 py-4 border-b border-slate-200">
@@ -70,36 +85,25 @@ const Statistics = () => {
                                             <div className="font-semibold text-left"> Matrícula </div>
                                         </th>
                                         <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S1_T </div>
+                                            <div className="font-semibold text-left"> Sección 1 </div>
                                         </th>
                                         <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S1_S </div>
+                                            <div className="font-semibold text-left"> Sección 2 </div>
                                         </th>
                                         <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S2_T </div>
+                                            <div className="font-semibold text-left"> Sección 3 </div>
                                         </th>
                                         <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S2_S </div>
+                                            <div className="font-semibold text-left"> Sección 4 </div>
                                         </th>
-                                        <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S3_T </div>
-                                        </th>
-                                        <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S3_S </div>
-                                        </th>
-                                        <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S4_T </div>
-                                        </th>
-                                        <th className="p-4 whitespace-nowrap"> 
-                                            <div className="font-semibold text-left"> S4_S </div>
-                                        </th>
+                                        
                                         
                                     </tr>
                                 </thead>
 
                                 { /* Table body */}
 
-                               <GeneralStatsRow setData={setTable3}/>
+                               <GeneralStatsRow option={{selectedOption}} setData={setTableProgress} />
                                 
                             </table>
                         </div>
