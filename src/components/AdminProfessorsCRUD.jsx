6ab2da356bae7e39b0d60fcee0674d4c91f057fa
h3cth0r <hectorsucre13@gmail.com>
2023-04-18 01:52:09 -0600

    fixed to add header panel in CRUDS

--- src/components/AdminProfessorsCRUD.jsx
+++ src/components/AdminProfessorsCRUD.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AdminSidebar from "./AdminSidebar";
 import CRUD_users from "./CRUD_users";
+import HeaderPanel from "./headerPanel";
 
 const AdminProfessorsCRUD = () => {
     return(
         <div className="flex bg-gray-100">
             <AdminSidebar/>
             <div className="flex flex-col w-full ml-56">
+                <HeaderPanel />
                 <CRUD_users type_user={"professors"}/>
             </div>
         </div>
--- src/components/PrubesComp.jsx
+++ src/components/PrubesComp.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import CRUD_users from "./CRUD_users";
 import EnableDBCard from "./EnbleDBCard";
 import AdminMainPanel from "./AdminMainPanel";
+import HeaderPanel from "./headerPanel";
 
 const PrubesComponent = () => {
     return(
         <div>
+            <HeaderPanel />
             <AdminMainPanel/>
         </div>
     );
