87c70f6dc0e49f4961cd94d71dbb30bd7a43c89e
h3cth0r <hectorsucre13@gmail.com>
2023-04-17 23:47:10 -0600

    Added admin general panel with enable/disable db, information card, error log and CRUD section for users and professors

---
+++ src/components/PrubesComp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CRUD_users from "./CRUD_users";
+import EnableDBCard from "./EnbleDBCard";
+import AdminMainPanel from "./AdminMainPanel";
+
+const PrubesComponent = () => {
+    return(
+        <div>
+            <AdminMainPanel/>
+        </div>
+    );
+};
+export default PrubesComponent;
