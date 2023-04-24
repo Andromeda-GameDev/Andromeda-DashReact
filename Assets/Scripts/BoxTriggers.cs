ccc572b6e30725c39de32a95e8a9ece52fe4f0ce
ShushoRiv <73494815+A01655181@users.noreply.github.com>
2023-04-23 22:46:40 -0600

    hotFix BoxTriggers
    ewe

--- Assets/Scripts/BoxTriggers.cs
+++ Assets/Scripts/BoxTriggers.cs
@@ -44,12 +44,12 @@ public class BoxTriggers : MonoBehaviour
         Debug.Log($"Input en start = {input}");
         if(input == 2) // too fast 
         {
+            failBox2.SetActive(true);
             winBox1.SetActive(true);
-            failBox1.SetActive(true);
         }
         else if(input == 1) // too slow
         {
-            failBox2.SetActive(true);
+            failBox1.SetActive(true);
         }
         else if(input == 0) // correct
         {
@@ -149,8 +149,7 @@ public class BoxTriggers : MonoBehaviour
             moving = false;
             animator.Play("LessSpeed"); // stop movement
             failBox1.SetActive(false);
-            SFXManager.instance.selectSFX(4);
-            //SFXManager.instance.selectSFX(1);
+            SFXManager.instance.selectSFX(1);
         }
         else if (name == "Drop_TB")
         {
@@ -161,6 +160,7 @@ public class BoxTriggers : MonoBehaviour
         {
             dropFail = true; // confirm fail flag
             failBox2.SetActive(false);
+            SFXManager.instance.selectSFX(4);
         }
     }
     
@@ -195,8 +195,6 @@ public class BoxTriggers : MonoBehaviour
             //tDrop astronaut as a ragdoll
             moving = false;
             animator.Play("LessSpeed");
-            SFXManager.instance.selectSFX(4);
-            SFXManager.instance.selectSFX(1);
         }
     }
 
