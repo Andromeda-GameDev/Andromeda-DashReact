5854ce4adb25576fa548223ebc75899335552d9a
H3cth0r <hectorsucre13@gmail.com>
2023-07-03 17:43:02 -0600

    added enable the objects from the parent object or not

--- Assets/Scenes/scripts/GeneralObjectController.cs
+++ Assets/Scenes/scripts/GeneralObjectController.cs
@@ -7,13 +7,20 @@ using UnityEngine;
  * */
 public class GeneralObjectController : MonoBehaviour
 {
-    public int startPoint_t       = -20;
-    public bool rotateXY_t        = true;
-    public string test_t          = "x ^ 2";
-    public int numDisks_t         = 50;
-    public int numShells_t        = 100;
-    public float height_t         = 5f;
-    public float cylinderHeight_t = 0.2f;
+    public int startPoint_t             = -20;
+    public bool rotateXY_t              = true;
+    public string test_t                = "x ^ 2";
+    public int numDisks_t               = 50;
+    public int numShells_t              = 100;
+    public float height_t               = 5f;
+    public float cylinderHeight_t       = 0.2f;
+    public float shellThicknessFactor_t = 2f;
+
+    public bool enableOuterShell        = true;
+    public bool enableInnerShell        = true;
+    public bool enableDisks             = true;
+    public bool enableOuterShellsMethod = true;
+    public bool enableInnerShellsMethod = true;
 
     private SolidOfRevolution       child1;
     private SolidOfRevolution       child2;
@@ -22,7 +29,7 @@ public class GeneralObjectController : MonoBehaviour
     private SolidOfRevolutionShell  child5;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
       Transform child1Transform = transform.Find("OuterShell");
       Transform child2Transform = transform.Find("InnerShell");
@@ -35,8 +42,9 @@ public class GeneralObjectController : MonoBehaviour
        * */
       if (child1Transform != null)
       {
+          child1Transform.gameObject.SetActive(enableOuterShell);
           child1  = child1Transform.GetComponent<SolidOfRevolution>();
-          if(child1 != null)
+          if(child1 != null && enableOuterShell)
           {
               child1.startPoint   = startPoint_t;
               child1.rotateXY     = rotateXY_t;
@@ -51,8 +59,9 @@ public class GeneralObjectController : MonoBehaviour
        * */
       if (child2Transform != null)
       {
+          child2Transform.gameObject.SetActive(enableInnerShell);
           child2  = child2Transform.GetComponent<SolidOfRevolution>();
-          if(child2 != null)
+          if(child2 != null && enableInnerShell)
           {
               child2.startPoint   = startPoint_t;
               child2.rotateXY     = rotateXY_t;
@@ -67,8 +76,9 @@ public class GeneralObjectController : MonoBehaviour
        * */
       if(child3Transform != null)
       {
+          child3Transform.gameObject.SetActive(enableDisks);
           child3 = child3Transform.GetComponent<SolidOfRevolutionDisks>();
-          if(child3 != null)
+          if(child3 != null && enableDisks)
           {
               child3.startPoint     = startPoint_t;
               child3.rotateXY       = rotateXY_t;
@@ -84,14 +94,16 @@ public class GeneralObjectController : MonoBehaviour
        * */
       if(child4Transform != null)
       {
+        child4Transform.gameObject.SetActive(enableOuterShellsMethod);
         child4 = child4Transform.GetComponent<SolidOfRevolutionShell>();
-        if(child4 != null)
+        if(child4 != null && enableOuterShellsMethod)
         {
-              child4.startPoint     = startPoint_t;
-              child4.rotateXY       = rotateXY_t;
-              child4.test           = test_t;
-              child4.numShells      = numShells_t;
-              child4.solidLength    = height_t;
+              child4.startPoint             = startPoint_t;
+              child4.rotateXY               = rotateXY_t;
+              child4.test                   = test_t;
+              child4.numShells              = numShells_t;
+              child4.solidLength            = height_t;
+              child4.shellThicknessFactor   = shellThicknessFactor_t;
         }
       }
 
@@ -100,14 +112,16 @@ public class GeneralObjectController : MonoBehaviour
        * */
       if(child5Transform != null)
       {
+        child5Transform.gameObject.SetActive(enableInnerShellsMethod);
         child5 = child5Transform.GetComponent<SolidOfRevolutionShell>();
-        if(child5 != null)
+        if(child5 != null && enableInnerShellsMethod)
         {
-              child5.startPoint     = startPoint_t;
-              child5.rotateXY       = rotateXY_t;
-              child5.test           = test_t;
-              child5.numShells      = numShells_t;
-              child5.solidLength    = height_t;
+              child5.startPoint             = startPoint_t;
+              child5.rotateXY               = rotateXY_t;
+              child5.test                   = test_t;
+              child5.numShells              = numShells_t;
+              child5.solidLength            = height_t;
+              child5.shellThicknessFactor   = shellThicknessFactor_t;
         }
       }
         
