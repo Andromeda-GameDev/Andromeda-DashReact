3655e83934374642b65cc2d7234bd48d0b71b98e
ShushoRiv <73494815+A01655181@users.noreply.github.com>
2023-07-06 13:05:10 -0600

    Empty object added
    Agregue un empty.

---
+++ Assets/Scenes/Level2/test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Yo soy un test :p");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
