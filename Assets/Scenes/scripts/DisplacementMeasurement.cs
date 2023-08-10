afc962f87f432cd9e80e7c3acac3856789f0cba5
H3cth0r <hectorsucre13@gmail.com>
2023-08-09 22:50:16 -0600

    Updated .gitignore, added AR session and working on z axis displacement script

--- Assets/Scenes/scripts/DisplacementMeasurement.cs
+++ Assets/Scenes/scripts/DisplacementMeasurement.cs
@@ -1,52 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 public class DisplacementMeasurement : MonoBehaviour
 {
-    
-    [SerializeField] ARRaycastManager arRaycastManager;
-    [SerializeField] Camera arCamera;
+    private Vector3 initialPosition;
+    public Text zDisplacementText;
+    public float zDisplacement;
 
-    Vector3 initialPointPosition;
-    bool measuring = false;
-    float displacementZ;
-
-    // Start is called before the first frame update
     void Start()
     {
-        
+        // Store the initial position of the camera or AR device
+        initialPosition = Camera.main.transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-       if(Input.touchCount > 0)
-       {
-          Touch touch   = Input.GetTouch(0);
+        // Get the current camera position
+        Vector3 currentCameraPosition = Camera.main.transform.position;
 
-          if(touch.phase == TouchPhase.Began)
-          {
-            List<ARRaycastHit> hits = new List<ARRaycastHit>();
-            if (arRaycastManager.Raycast(touch.position, hits, UnityEngine.XR.ARSubsystems.TrackableType.FeaturePoint)) 
-            {
-              initialPointPosition = hits[0].pose.position;
-              measuring = true;
-            }
-          }
-       }
+        // Calculate the displacement in the z-axis
+        zDisplacement = currentCameraPosition.z - initialPosition.z;
 
-      if(measuring)
-      {
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
-        if (arRaycastManager.Raycast(Input.mousePosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.FeaturePoint))
-        {
-          Vector3 currentPosition = hits[0].pose.position;
-          displacementZ = currentPosition.z - initialPointPosition.z;
-
-          Debug.Log("Displacement in Z: " + displacementZ);
-        }
-      }
+        // Display the displacement in the console
+        Debug.Log("Z Displacement: " + zDisplacement);
+        zDisplacementText.text = "Z Displacement: " + zDisplacement;
     }
 }
