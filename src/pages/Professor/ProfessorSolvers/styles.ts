3e950423612a303db67524e2a1ea06d92346baff
H3cth0r <hectorsucre13@gmail.com>
2023-08-23 18:53:19 -0600

    added levels solvers for teachers

---
+++ src/pages/Professor/ProfessorSolvers/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const FlexContainer = styled.div`
+  display: flex;
+  background-color: #E5E7EB; 
+`;
+
+export const OpenButton = styled.button`
+  border: none;
+  border-radius: 9999px;
+  background-color: #3B82F6; 
+  color: #FFFFFF; 
+  padding: 0.5rem 1rem; 
+  cursor: pointer;
+`;
---
+++ src/pages/Professor/ProfessorSolvers/types.ts
@@ -0,0 +1,6 @@
+export type DataItem = {
+  id: string;
+  name: string;
+  url: string; // Change to the appropriate type for the URL
+};
+
